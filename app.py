8baa3391c0439df60e31704b5a6f846dc454a6e2
sachin-h-s <89412649+sachin-h-s@users.noreply.github.com>
2021-08-25 14:36:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,18 +9,12 @@ st.markdown('''
 # **The EDA App**
 
 This is the **EDA App** created in Streamlit using the **pandas-profiling** library.
-
-**Credit:** App built in `Python` + `Streamlit` by [Chanin Nantasenamat](https://medium.com/@chanin.nantasenamat) (aka [Data Professor](http://youtube.com/dataprofessor))
-
----
 ''')
 
 # Upload CSV data
 with st.sidebar.header('1. Upload your CSV data'):
     uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-    st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
-""")
+
 
 # Pandas Profiling Report
 if uploaded_file is not None:
